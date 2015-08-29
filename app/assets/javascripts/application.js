77501704778358d8d246c4594fe704f48ccb1a09
quyba <quybadesign@gmail.com>
2015-08-29 16:19:29 -0500

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+
+
+//= require modernizr.js
+//= require isotope/isotope.pkgd.min.js
+//= require 'jquery.backstretch'
+//= require jquery.appear.js
+//= require template.js
+//= require custom.js
+//= require jquery.easing.1.3.js
+//= require jquery.booklet.latest.min.js
+//= require unslider.min.js
+
+//= require turn.js
+//= require turn.html4.min.js
+//= require_tree .
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  def index
+  end
+  
+  def catalogo
+    
+  end
+  
+  def brigadas
+    
+  end
+  
+  def nosotros
+    
+  end
+  
+  def contactenos
+    
+  end
+  
+end
